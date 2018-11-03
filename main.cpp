6e17dd2257bea4aa14b7f3f32beaaddf6e28b6bc
Audrey Corbeil Therrien <therria@slac.stanford.edu>
2018-11-02 17:09:04 -0700

    Modified the event array to a uint8 vector to eventually use switch case startegy. Modified the data array to a NPIXEL by X streaming array

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <Dense>
-#include <deque>
+#include <vector>
 #include <chrono>
 #include <unistd.h>
+#include <random>
 
 using namespace Eigen;
 using namespace std;
@@ -11,10 +12,18 @@ using namespace std;
 #define VERBOSE 2
 #define REPEAT 3
 #define SAMPLESPERWINDOW 100
-#define MAXWINDOW 100
-#define MAXSAMPLESIZE MAXWINDOW*SAMPLESPERWINDOW
+#define WINDOWS_IN_BUFFER 100
+#define MAXSAMPLESIZE WINDOWS_IN_BUFFER*SAMPLESPERWINDOW
 #define MAXFILTERS 3
 #define NUMEVENTS 1000
+#define NPIXEL 250
+
+const uint32_t CASE1 = 16843008;
+const uint32_t CASE2 = 65792;
+const uint32_t CASE3 = 16842753;
+const uint32_t CASE4 = 65537;
+const uint32_t CASE5 = 16842752;
+const uint32_t CASE6 = 65536;
 
 float evaluate_energy(const ArrayXi int_samples, const ArrayXXf filters, int windowsize){
 
@@ -42,29 +51,53 @@ float evaluate_energy(const ArrayXi int_samples, const ArrayXXf filters, int win
 
 }
 
-int make_data(ArrayXi *samples, int new_samples = -1, int start_index=0){
+void make_events(vector<uint8_t> hits[NPIXEL], int total, float threshold=0.5){
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution<double> dist;
+        for (int p=0; p<NPIXEL; p++) {
+            for (int i=0; i<total; i++) {
+                double random = dist(gen);
+                if (random > threshold) {
+                    hits[p][i] = 1;
+                }
+                else {
+                    hits[p][i] = 0;
+                }
+            }
+        }
+}
+
+int make_data(ArrayXXi *samples, int new_samples = -1, int start_index=0){
     //fills the array with random data. can specify width
     // returns new write index
         if (new_samples == -1){
-        new_samples = (*samples).size();
+        new_samples = (*samples).cols();
+        cout << "size : " << new_samples <<endl;
         }
 
         if (start_index+new_samples >= (*samples).size()){
             int partial_samples = (*samples).size()-start_index;
-            (*samples).block(start_index,0,partial_samples,1) = ArrayXi::Random(partial_samples, 1);
+            (*samples).block(0,start_index,NPIXEL,partial_samples) = ArrayXXi::Random(NPIXEL, partial_samples);
             partial_samples = new_samples-partial_samples;
-            (*samples).block(start_index,0,partial_samples,1) = ArrayXi::Random(partial_samples, 1);
+            (*samples).block(0,0,NPIXEL,partial_samples) = ArrayXXi::Random(NPIXEL, partial_samples);
         }
         else {
-            (*samples).block(start_index,0,new_samples,1) = ArrayXi::Random(new_samples, 1);
+            (*samples).block(0,start_index,NPIXEL, new_samples) = ArrayXXi::Random(NPIXEL, new_samples);
         }
 
         return (start_index+new_samples)%((*samples).size());
 }
 
+
 int main(int argc, char** argv)
 {
-    ArrayXi samples(MAXSAMPLESIZE);
+    ArrayXXi samples(NPIXEL, MAXSAMPLESIZE);
+    std::vector<uint8_t> hits[NPIXEL];
+    for (int i=0; i<NPIXEL; i++) {
+        hits[i].resize(MAXSAMPLESIZE);
+    }
+    make_events(hits, NUMEVENTS);
     int write_index = make_data(&samples);
     int read_index=0;
     bool wraparound=true;
@@ -73,60 +106,32 @@ int main(int argc, char** argv)
     ArrayXXf selectedFilters(MAXSAMPLESIZE, FILTERS);
     int first_index=0;
     int last_index=100;
-    ArrayXXf tempEvents = ArrayXXf::Random(1, NUMEVENTS);
-    Array<bool, 1, NUMEVENTS> isEvent;
 
-    isEvent = tempEvents > 0.5;
+
 
     int sum=0;
 
     auto t1 = std::chrono::high_resolution_clock::now();
-//    for (int repeat = 0; repeat<REPEAT; repeat++){
         read_index = 3*SAMPLESPERWINDOW;
         for (int i = 3; i<NUMEVENTS-1; i++){
-            if (wraparound == false && (write_index - read_index <= 2*SAMPLESPERWINDOW)){
-                write_index = make_data(&samples, (MAXWINDOW/2) * SAMPLESPERWINDOW, write_index);
+            if (wraparound == false && (write_index - read_index <= 2*WINDOWS_IN_BUFFER)){
+                write_index = make_data(&samples, (WINDOWS_IN_BUFFER/2) * SAMPLESPERWINDOW, write_index);
                 cout << "Index updated to : " << write_index << endl;
                 if (read_index > write_index) { wraparound = true; }
             }
-            if (wraparound == true && (samples.size()-(read_index - write_index) <= 2*SAMPLESPERWINDOW)){
-                write_index = make_data(&samples, (MAXWINDOW/2) * SAMPLESPERWINDOW, write_index);
+            if (wraparound == true && (samples.size()-(read_index - write_index) <= 2*WINDOWS_IN_BUFFER)){
+                write_index = make_data(&samples, (WINDOWS_IN_BUFFER/2) * SAMPLESPERWINDOW, write_index);
                 cout << "Index updated to : " << write_index << endl;
             }
-            if (isEvent(i) == true){
-                if (isEvent(i+1) == true){
-                    //use filter of group A - ignore following window
-                    selectedFilters = filterGroupA;
-                    last_index = read_index+SAMPLESPERWINDOW;
-                }
-                else{
-                    //use filter of group B, include following window
-                    selectedFilters = filterGroupB;
-                    last_index = read_index+2*SAMPLESPERWINDOW;
-                }
 
-                if (isEvent(i-1) == true){
-                    //Signal in previous window, include this window
-                    first_index = read_index-SAMPLESPERWINDOW;
-                }
-                else{
-                    //No signal in previous window, use window before that, different filter if there is a signal or not
-                    first_index = read_index-2*SAMPLESPERWINDOW;
-                    if (isEvent(i-2) == true){
-                    }
-                    else{
-                    }
-                }
-            cout << isEvent.segment<5>(i-3) << " -- " << first_index << "-" << last_index << endl;
-            }
-            read_index += 100;
+            //Increment the data reader
+            read_index+= SAMPLESPERWINDOW;
             if (read_index>samples.size()){
                 read_index=0;
                 wraparound=false;
             }
-
         }
- //   }
+
 
 
     auto t2 = std::chrono::high_resolution_clock::now();
